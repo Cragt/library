b1d5978e34caaa3193040b2954e44252b9805d40
Craig <cragt369@gmail.com>
2023-01-17 17:06:47 -0600

    Finish add book button functionality, start on remove book button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("newBook");
 const modalBtn = document.getElementById("modal-btn");
-const title = document.getElementById("title").value;
-const author = document.getElementById("author").value;
-const pages = document.getElementById("pages").value;
-const haveRead = document.getElementById("have-read").value;
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const haveRead = document.getElementById("have-read");
 const span = document.getElementsByClassName("close")[0];
 
 let myLibrary = [];
@@ -25,6 +25,13 @@ const book2 = new Book("ncfom", "Cormac McCarthy", 435, "read");
 const book3 = new Book("LoTR", "That dude", 13223, "unread");
 const book4 = new Book("Fireflies", "C. McCarn", 231, "read");
 
+addBooKToLibrary(book1);
+
+addBooKToLibrary(book2);
+
+addBooKToLibrary(book3);
+addBooKToLibrary(book4);
+
 function addBooKToLibrary(book) {
   const cards = document.getElementById("cards");
   const card = document.createElement("div");
@@ -32,6 +39,9 @@ function addBooKToLibrary(book) {
   const aNode = document.createElement("p");
   const pNode = document.createElement("p");
   const rNode = document.createElement("p");
+  card.classList.add("card");
+  // const removeBtn = document.createElement('button');
+  // removeBtn.classList.add('btn');
   const titleNode = document.createTextNode(book.title);
   const authorNode = document.createTextNode(book.author);
   const pagesNode = document.createTextNode(book.pages);
@@ -40,18 +50,13 @@ function addBooKToLibrary(book) {
   aNode.appendChild(authorNode);
   pNode.appendChild(pagesNode);
   rNode.appendChild(readNode);
+  // card.appendChild(removeBtn);
   card.appendChild(tNode);
   card.appendChild(aNode);
   card.appendChild(pNode);
   card.appendChild(rNode);
   cards.appendChild(card);
 }
-
-addBooKToLibrary(book1);
-
-addBooKToLibrary(book2);
-
-addBooKToLibrary(book3);
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -67,8 +72,16 @@ window.onclick = function (event) {
   }
 };
 
-modalBtn.onclick = function () {
-  const book = new Book(title, author, pages, haveRead);
-  addBooKToLibrary(book);
-  modal.style.display = "none";
-};
+// modalBtn.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   const book = new Book(title.value, author.value, pages.value, haveRead.value);
+//   addBooKToLibrary(book);
+//   modal.style.display = "none";
+// });
+
+// const removeBook = (e) => {
+//   e.target.replaceAll(
+//     ""
+//   )}
+
+//   document.getElementsByClassName("btn").onclick = removeBook;
