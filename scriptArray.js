39875a564967cff16d02fe6ebd5d228ee424da6a
Craig <cragt369@gmail.com>
2023-01-20 16:28:47 -0600

    --allow-empty

--- scriptArray.js
+++ scriptArray.js
@@ -5,8 +5,9 @@ const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const haveRead = document.getElementById("have-read");
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close");
 const cards = document.getElementById("cards");
+const removeBtn = document.getElementsByClassName("remove-btn");
 
 let myLibrary = [];
 
@@ -33,7 +34,7 @@ const book3 = new Book("Mickey Mouse", "J.K. Rowling", 123, "Unread");
 createCard = function () {
   cards.innerHTML = "";
   myLibrary.forEach((book) => {
-    let html = `<div class="card"><p>${book.title}</p><p>${book.author}</p><p>${book.pages}</p><p>${book.haveRead}</p><button class="remove-btn">Delete</div>`;
+    let html = `<div class="card"><p>${book.title}</p><p>${book.author}</p><p>${book.pages}</p><p>${book.haveRead}</p><button class="remove-btn" onclick="deleteBook(this)">Delete</div>`;
     cards.innerHTML += html;
   });
 };
@@ -43,8 +44,6 @@ function alreadyInLibrary(title) {
     return el.title === title;
   });
 }
-
-//Old stuff
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -64,70 +63,18 @@ modalBtn.addEventListener("click", function (event) {
   event.preventDefault();
   const book = new Book(title.value, author.value, pages.value, haveRead.value);
   modal.style.display = "none";
-  console.log(book.title);
   createCard();
 });
 
+function deleteBook(el) {
+  const element = el;
+  console.log();
+  element.parentNode.remove();
+}
+
+function removeFromArray() {}
+
 createCard();
 
-// function addBooKToLibrary(book) {
-//   myLibrary.push(book);
-//   createBookCard(book);
-// }
-
-// const book1 = new Book("Harry Potter", "J.K. Rowling", 123, "unread");
-// const book2 = new Book("ncfom", "Cormac McCarthy", 435, "read");
-// const book3 = new Book("LoTR", "That dude", 13223, "unread");
-// const book4 = new Book("Fireflies", "C. McCarn", 231, "read");
-// const book5 = new Book("Good Time Dragon", "C.T.", 1233, "unread");
-
-// function createBookCard(book) {
-//   const card = document.createElement("div");
-//   const tNode = document.createElement("p");
-//   const aNode = document.createElement("p");
-//   const pNode = document.createElement("p");
-//   const rNode = document.createElement("p");
-//   const titleNode = document.createTextNode(book.title);
-//   const authorNode = document.createTextNode(book.author);
-//   const pagesNode = document.createTextNode(book.pages);
-//   const readNode = document.createTextNode(book.haveRead);
-//   tNode.appendChild(titleNode);
-//   aNode.appendChild(authorNode);
-//   pNode.appendChild(pagesNode);
-//   rNode.appendChild(readNode);
-//   // card.appendChild(removeBtn);
-//   card.appendChild(tNode);
-//   card.appendChild(aNode);
-//   card.appendChild(pNode);
-//   card.appendChild(rNode);
-//   cards.appendChild(card);
-// }
-
-// btn.onclick = function () {
-//   modal.style.display = "block";
-// };
-
-// span.onclick = function () {
-//   modal.style.display = "none";
-// };
-
-// window.onclick = function (event) {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-// };
-
-// // modalBtn.addEventListener("click", function (event) {
-// //   event.preventDefault();
-// //   const book = new Book(title.value, author.value, pages.value, haveRead.value);
-
-// //        modal.style.display = "none";
-// //        addBooKToLibrary(book);
-// //   console.log(book.title);
-// // });
-
-// function alreadyInLibrary(title) {
-//   return myLibrary.some(function (el) {
-//     return el.title === title;
-//   });
-// }
+// const index = myLibrary.findIndex(v => v.title === "Harry Potter");
+// console.log(index);
